9e1695227fe80f05ac210e84f8c3a0ca5660b7f9
danillo55 <danymad55@gmail.com>
2025-11-24 16:34:24 +0700

    build 24112025

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -4,9 +4,9 @@ using UnityEngine.SceneManagement;
 
 public class MusicPlayer : MonoBehaviour
 {
-    private static MusicPlayer instance;
+   private static MusicPlayer instance;
 
-    void Awake()
+ /*    void Awake()
     {
         string currentSceneName = SceneManager.GetActiveScene().name;
 
@@ -21,8 +21,50 @@ public class MusicPlayer : MonoBehaviour
             currentSceneName == "Main Menu Rainy's Episode" ||
             currentSceneName == "Main Menu Real's Episode")
         {
+            Debug.Log("Music player destroyed");
             Destroy(gameObject);
             return;
+
+        }
+        // Set this as the singleton instance
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }*/
+    void OnEnable()
+    {
+        // Subscribe to sceneLoaded event
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        // Unsubscribe to avoid memory leaks
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+
+        // If another MusicPlayer already exists, destroy this one
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            
+        }
+
+        if (currentSceneName == "Main Menu Jo's Episode" || 
+            currentSceneName == "Main Menu Rainy's Episode" ||
+            currentSceneName == "Main Menu Real's Episode")
+        {
+
+            if (instance == null) {
+                Debug.Log("Music Player is already destroyed");
+            }
+            else{
+                Debug.Log("Music player destroyed");
+                Destroy(gameObject);
+            }
 
         }
         // Set this as the singleton instance
