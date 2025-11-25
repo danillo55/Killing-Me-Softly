6758bcf9980be22a509708d237e3db2bcb3200db
danillo55 <danymad55@gmail.com>
2025-11-25 14:42:59 +0700

    fix Moonwalk, some dialogs, heartbeat,

--- Assets/Scripts/BackgroundManager.cs
+++ Assets/Scripts/BackgroundManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using DG.Tweening; // Import DOTween namespace
 using System; 
+using UnityEngine.SceneManagement;
 
 [System.Serializable]
 public class BackgroundList
@@ -39,15 +40,17 @@ public class BackgroundManager : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        string currentSceneName = SceneManager.GetActiveScene().name;
     //    GameObject center2D = GetObjectAtLeft();
+        if (currentSceneName == "Scene 6")
+        {
+            Background1.background.transform.Translate(Vector3.left * Time.deltaTime * CameraSpeed);
+            Background2.background.transform.Translate(Vector3.left * Time.deltaTime * CameraSpeed);
+            Background3.background.transform.Translate(Vector3.left * Time.deltaTime * CameraSpeed);
+            Background4.background.transform.Translate(Vector3.left * Time.deltaTime * CameraSpeed);
+            Background5.background.transform.Translate(Vector3.left * Time.deltaTime * CameraSpeed);
 
-        Background1.background.transform.Translate(Vector3.left * Time.deltaTime * CameraSpeed);
-        Background2.background.transform.Translate(Vector3.left * Time.deltaTime * CameraSpeed);
-        Background3.background.transform.Translate(Vector3.left * Time.deltaTime * CameraSpeed);
-        Background4.background.transform.Translate(Vector3.left * Time.deltaTime * CameraSpeed);
-        Background5.background.transform.Translate(Vector3.left * Time.deltaTime * CameraSpeed);
-
-        if(Background1.background.transform.position.x <= -25.6f)
+            if(Background1.background.transform.position.x <= -25.6f)
             {
                 Background1.background.transform.DOMove(new Vector3(38.4f, 0, -4), 0.0001f);
 
@@ -58,66 +61,33 @@ public class BackgroundManager : MonoBehaviour
                 Background3 = Background4;
                 Background4 = Background5; 
                 Background5 = tempBackground;
-
             }
-
-    /*    GameObject center2D = GetObjectAtScreenCenter2D();
-    
-    if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-        {
-            Debug.Log("2D center object: " + center2D.name);
-
-            mainCamera.transform.Translate(Vector3.left * Time.deltaTime * CameraSpeed);
-
-            if(center2D == LeftBackground.background)
-            {
-                RightBackground.background.transform.DOMove(new Vector3(RightBackground.background.transform.position.x - 64,
-                                                        RightBackground.background.transform.position.y,
-                                                        RightBackground.background.transform.position.z), 0.0001f);
-
-                LeftBackground = RightBackground;
-
-                if (RightBackground == background[0])
-                {
-                    RightBackground = background[4];
-                }
-                else
-                {
-                    RightBackground = background[Array.IndexOf(background, RightBackground) - 1]; 
-                }
-            }
-       
 
         }
-
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        else if (currentSceneName == "Scene 23")
         {
-            mainCamera.transform.Translate(Vector3.right * Time.deltaTime * CameraSpeed);
-            Debug.Log("2D center object: " + center2D.name);
-
-            if(center2D == RightBackground.background)
+            Background1.background.transform.Translate(Vector3.right * Time.deltaTime * CameraSpeed);
+            Background2.background.transform.Translate(Vector3.right * Time.deltaTime * CameraSpeed);
+            Background3.background.transform.Translate(Vector3.right * Time.deltaTime * CameraSpeed);
+            Background4.background.transform.Translate(Vector3.right * Time.deltaTime * CameraSpeed);
+            Background5.background.transform.Translate(Vector3.right * Time.deltaTime * CameraSpeed);
+            
+            if(Background5.background.transform.position.x >= 25.6f)
             {
-                LeftBackground.background.transform.DOMove(new Vector3(LeftBackground.background.transform.position.x + 64,
-                                                        LeftBackground.background.transform.position.y,
-                                                        LeftBackground.background.transform.position.z), 0.0001f);
+                Background5.background.transform.DOMove(new Vector3(-38.4f, 0, -4), 0.0001f);
 
-                RightBackground = LeftBackground;
+                BackgroundList tempBackground = Background5;
 
-                if (LeftBackground == background[4])
-                {
-                    LeftBackground = background[4];
-                }
-                else
-                {
-                    LeftBackground = background[Array.IndexOf(background, LeftBackground) + 1]; 
-                }
+                Background5 = Background4;
+                Background4 = Background3;
+                Background3 = Background2;
+                Background2 = Background1; 
+                Background1 = tempBackground;
             }
-        }*/
-
+        }
+        
 
     }
-
-    // 2D: requires Collider2D on sprites
     private GameObject GetObjectAtScreenCenter2D()
     {
         if (mainCamera == null) return null;
@@ -127,13 +97,13 @@ public class BackgroundManager : MonoBehaviour
         return col ? col.gameObject : null;
     }
 
-    private GameObject GetObjectAtLeft()
+/*    private GameObject GetObjectAtLeft()
     {
         if (secondCamera == null) return null;
         Vector3 screenLeft = new Vector3(secondCamera.transform.position.x, secondCamera.transform.position.y, Mathf.Abs(secondCamera.transform.position.z));
         Vector3 leftPoint = mainCamera.ScreenToWorldPoint(screenLeft);
         Collider2D col = Physics2D.OverlapPoint(new Vector2(leftPoint.x, leftPoint.y));
         return col ? col.gameObject : null;
-    }
+    }*/
 
 }
